d3bc0c2b020e8dfb26afce3a6c4614b13ca140a2
Paulo Araújo <90206752+86Paulo@users.noreply.github.com>
2022-04-04 15:58:04 +0100

    Media query done and JS done

---
+++ app/orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNmber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Larvander kf102 Drone',
+        productNmber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko f11 pro drone',
+        productNmber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNmber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
